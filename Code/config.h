f63b91cb1bc31f9efe390db4e34c76e94d5db08f
Pan0rama <u.amerkamp@freenet.de>
2020-11-21 02:56:51 +0100

    Create config.h
    Anmeldeinformationen für WiFi und Thingspeak ausgelagert. Nur in esp8266 Version

---
+++ Code/config.h
@@ -0,0 +1,7 @@
+// In dieser Datei werden alle Verbindungsinfos ausgelagert
+
+#define SECRET_SSID "MySSID"    // SSID des WLAN
+#define SECRET_PASS "MyPassword"  // PSW 
+
+#define SECRET_CH_ID 000000     // thingspeak channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // thingspeak channel write API Key
